f65c35d495d4dea60325785c837b8dba337476de
Thomas Noe <thomas.noe@madwiremedia.com>
2017-12-31 12:07:38 -0700

    recursive version

--- apps/one/lib/one.ex
+++ apps/one/lib/one.ex
@@ -3,18 +3,18 @@ defmodule One do
   advent of code, day 1
   """
   def captcha(nums) do
-    l = Integer.to_string(nums) 
+    split_and_indexed = Integer.to_string(nums) 
         |> String.graphemes 
         |> Enum.map(fn x -> String.to_integer(x) end)
         |> Enum.with_index
-    l
+    split_and_indexed 
       |> Enum.filter(fn x ->
           {val, i} = x
-          n = case Enum.fetch(l, i + 1) do
+          n = case Enum.fetch(split_and_indexed, i + 1) do
             {:ok, next} ->
               next
             :error -> 
-              Enum.at(l, 0)
+              Enum.at(split_and_indexed, 0)
           end
 
           {nval, _} = n
@@ -25,5 +25,31 @@ defmodule One do
         val
       end)
       |> Enum.sum
+  end
+
+  def captcha2(nums) do
+    li = Integer.to_string(nums)
+    |> String.graphemes
+    |> Enum.map(&String.to_integer/1)
+    [start | _] = li
+    worker(li, 0, start)
+  end
+  def worker([head | []], acc, start_head) do
+    case head == start_head do
+      :true ->
+        acc + head
+      :false ->
+        acc
+    end
+  end
+  def worker([head | tail], acc, start_head) do
+    [next_head | _] = tail
+    acc = case head == next_head do
+      :true ->
+        acc + head
+      :false ->
+        acc
+    end
+    worker(tail, acc, start_head)
   end
 end
