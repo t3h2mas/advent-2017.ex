d4f20b383bca1c01ba047a39d4bbcccddc70385f
Thomas Noe <thomas.noe@madwiremedia.com>
2017-12-31 12:07:50 -0700

    test captcha2

--- apps/one/test/one_test.exs
+++ apps/one/test/one_test.exs
@@ -1,16 +1,29 @@
 defmodule OneTest do
   use ExUnit.Case
 
-  test "return of 1122" do
+  test "captcha return of 1122" do
     assert One.captcha(1122) == 3
   end
-  test "return of 1111" do
+  test "captcha return of 1111" do
     assert One.captcha(1111) == 4
   end
-  test "return of 1234" do
+  test "captcha return of 1234" do
     assert One.captcha(1234) == 0
   end
-  test "return of 91212129" do
+  test "captcha return of 91212129" do
     assert One.captcha(91212129) == 9
+  end
+
+  test "catpcha2 return of 1122" do
+    assert One.captcha2(1122) == 3
+  end
+  test "captcha2 return of 1111" do
+    assert One.captcha2(1111) == 4
+  end
+  test "captcha2 return of 1234" do
+    assert One.captcha2(1234) == 0
+  end
+  test "captcha2 return of 91212129" do
+    assert One.captcha2(91212129) == 9
   end
 end
