4e4a2c7b90dd3821d8864d4846ed97cb80df3327
Thomas Noe <thomas.noe@madwiremedia.com>
2018-01-10 18:15:29 -0700

    solve day 5 part 1

---
+++ apps/five/lib/five.ex
@@ -0,0 +1,53 @@
+defmodule Five do
+  @moduledoc """
+  Advent of Code, day five
+  """
+
+  @input_file "five.txt"
+
+  def read_input_file do
+    case File.read(@input_file) do
+      {:ok, body} ->
+        body
+      {:error, reason} ->
+        raise reason
+    end
+  end
+
+
+  def parse_jumps(jumps) do
+    jumps
+    |> String.split("\n", trim: :true)
+    |> Enum.map(fn x ->
+      x
+      |> String.trim
+      |> String.to_integer
+    end)
+  end
+
+  def cycle_jumps(jumps) do
+    start_index = 0
+    step = 0
+
+    cycle_jumps(jumps, start_index, step) 
+  end
+
+  def cycle_jumps(jumps, index, step) do
+    case Enum.fetch(jumps, index) do
+      {:ok, pointer_val} ->
+        next_jumps = List.replace_at(jumps, index, pointer_val + 1)
+        cycle_jumps(next_jumps, index + pointer_val, step + 1)
+      :error -> step
+    end
+  end
+
+  def solve_part_one do
+    IO.inspect(read_input_file()) |> solve_part_one
+  end
+
+  def solve_part_one(str) do 
+    str
+    |> parse_jumps
+    |> cycle_jumps
+  end
+end
